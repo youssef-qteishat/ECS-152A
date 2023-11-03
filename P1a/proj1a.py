90597c5bc936d5368fd0edd0958748173a747024
Youssef Qteishat <youssef.qt1@gmail.com>
2023-11-03 16:28:21 -0700

    Finalized Project 1a and Project 1b implementation

--- P1a/proj1a.py
+++ P1a/proj1a.py
@@ -2,56 +2,97 @@ import dpkt
 import sys
 import datetime
 
+# only list packets that are related to activity
 def parse_pcap(pcap_file):
+
+    # read the pcap file
+    f = open(pcap_file, 'rb')
+    pcap = dpkt.pcap.Reader(f)
+
     http_count = 0
     https_count = 0
+    FTC_count = 0
     FTP_count = 0
     ICMP_count = 0
+    ARP_count = 0
     SSH_count = 0
 
-    # Use context manager for file operations
-    with open(pcap_file, 'rb') as f:
-        pcap = dpkt.pcap.Reader(f)
+    # iterate over packets
+    for timestamp, data in pcap:
 
-        for timestamp, data in pcap:
-            try:
-                dest_ip = dpkt.utils.inet_to_str(ip.dst)
-                print(dest_ip)
-                eth = dpkt.ethernet.Ethernet(data)
-                if not isinstance(eth.data, (dpkt.ip.IP, dpkt.ip6.IP6)):
-                    continue
+        # convert to link layer object
+        eth = dpkt.ethernet.Ethernet(data)
 
-                ip = eth.data
-                if isinstance(ip.data, dpkt.icmp.ICMP):
-                    ICMP_count += 1
+        # do not proceed if there is no network layer data
+        if not isinstance(eth.data, dpkt.ip.IP) and not isinstance(eth.data, dpkt.ip6.IP6):
+            continue
 
-                # Check for TCP in the transport layer
-                if isinstance(ip.data, dpkt.tcp.TCP):
-                    tcp = ip.data
+        # extract network layer data
+        ip = eth.data
 
-                    # # Skip if there is no application layer data
-                    # if len(tcp.data) <= 0:
-                    #     continue
+        if isinstance(ip.data, dpkt.icmp.ICMP):
+            ICMP_count += 1
+            # icmp = ip.data
 
-                    # Check for specific application layer protocols
-                    if tcp.dport == 80 or tcp.sport == 80:
-                        http_count += 1
-                    elif tcp.dport == 443 or tcp.sport == 443:
-                        https_count += 1
-                    elif tcp.dport == 22 or tcp.sport == 22:
-                        SSH_count += 1
-                    elif tcp.dport in (20, 21) or tcp.sport in (20, 21):
-                        FTP_count += 1
+        #destination IP adress in IPv4 format
+        src_ip = dpkt.utils.inet_to_str(ip.src)
+        dst_ip = dpkt.utils.inet_to_str(ip.dst)
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
+        print("Source IP: ", src_ip)
+        print("Destination IP: ", dst_ip)
+       
+        # Dont procced if there is no transport layer data
+        if not isinstance(ip.data, dpkt.tcp.TCP):
+            continue
 
-    # Print the counts
+        
+        # extract transport layer data
+        tcp = ip.data
+
+        # do not proceed if there is no application layer data
+        # here we check length because we don't know protocol yet
+        if not len(tcp.data) > 0:
+            continue
+
+        # extract application layer data
+        # HTTP, HTTPS, SSH, FTC, FTP, ICMP, 
+
+        #########################################################################################################################################################
+
+        # HTTP Request or HTTP Response
+        # print(tcp.dport)
+        # print(tcp.sport)
+        if tcp.dport == 80 or tcp.sport == 80 or tcp.dport == 8080 or tcp.sport == 8080: 
+             http_count += 1
+        
+        #https:
+        if tcp.dport == 443 or tcp.sport == 443:
+            https_count += 1
+            
+        #SSH:
+        if tcp.dport == 22 or tcp.sport == 22:
+            SSH_count += 1
+        
+        #FTP:
+        if tcp.dport == 21 or tcp.dport == 20 or tcp.sport == 20 or tcp.sport == 21:
+            FTP_count += 1
+
+        #########################################################################################################################################################
+        print ('Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
+        print("IPV4: ", dest_ip)
     print("HTTP: ", http_count)
     print("HTTPS: ", https_count)
     print("ICMP: ", ICMP_count)
     print("FTP: ", FTP_count)
     print("SSH: ", SSH_count)
+            
+# parse_pcap("google_ping (1).pcap")
+# parse_pcap("example_ping.pcap")
+# parse_pcap("http_forever.pcap")
+# parse_pcap("FTP_wireshark.pcap")
+parse_pcap("CSIF_wireshark.pcap")
 
-# Replace with your pcap file path
-parse_pcap("example_ping.pcap")
+# 2. Visit https://example.com in your browser.
+# 3. Visit http://httpforever.com in your browser
+# 4. Access a FTP server (Type “ftp ftp.gnu.org” in your terminal)
+# 5. ssh into a CSIF machine
--- P1b/proj1b.py
+++ P1b/proj1b.py
@@ -1,8 +1,9 @@
 import dpkt
-import sys
+import socket
 import json
 
 def extract_json_from_http(packet_data):
+    
     try:
         # Parse the HTTP request or response
         http_request = dpkt.http.Request(packet_data)
@@ -22,27 +23,25 @@ def extract_json_from_http(packet_data):
 
 
 def sherlock(pcap_file):
+    counter = 1
     with open(pcap_file, 'rb') as f:
         pcap = dpkt.pcap.Reader(f)
 
         for timestamp, data in pcap:
             try:
                 eth = dpkt.ethernet.Ethernet(data)
-                # Extracting the source MAC address and converting it to a readable format
-                src_mac = ':'.join('%02x' % b for b in eth.src)
-                dst_mac = ':'.join('%02x' % b for b in eth.dst)
-                print("source: ", src_mac)
-                print("destinaton: ", dst_mac)
+                
+                
 
                 if isinstance(eth.data, (dpkt.ip.IP, dpkt.ip6.IP6)):
 
                     ip = eth.data
-                    if isinstance(ip.data, dpkt.icmp.ICMP):
-                        print("ICMP Type:", ip.data.type)
-                        print("ICMP Code:", ip.data.code)
-                        payload = bytes(ip.data.data)
-                        print(payload.decode('utf-8'))
 
+                    if (isinstance(ip.data, dpkt.icmp.ICMP)):
+                        src_ip = socket.inet_ntoa(ip.src)
+                        dst_ip = socket.inet_ntoa(ip.dst)
+                        print(counter, src_ip, dst_ip, sep=", ")
+                        counter += 1
 
                     if isinstance(ip.data, dpkt.tcp.TCP):
 
@@ -54,12 +53,6 @@ def sherlock(pcap_file):
                                 for line in lines:
                                     if 'secret' in line:
                                         print(line)
-                        # tcp = ip.data
-                        # json_data = extract_json_from_http(tcp.data)
-
-                        # if json_data:
-                        #     print(json_data)
-
             except Exception as e:
                 print(f"An error occurred: {e}")
 
